a35542d55a73930dd2f24518a713180edba79086
Edwin Watkeys <edw@poseur.com>
2014-08-10 12:18:15 -0400

    add administrivia

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject async.combinators "0.1.0-SNAPSHOT"
+  :description "Asynchronous combinators"
+  :url "http://github.com/edw/async.combinators"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]])
